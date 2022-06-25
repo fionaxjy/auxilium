1a4b5c6d9c0383ed434f830ad0f721a8ee8c2cc2
fionaxjy <fionaxiao2002@gmail.com>
2022-06-25 12:11:32 +0800

    Update causes.dart

--- lib/causes.dart
+++ lib/causes.dart
@@ -7,73 +7,121 @@ class CausesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Causes', style: TextStyle(color: Colors.black)),
-          backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Causes', style: TextStyle(color: Colors.black)),
+        backgroundColor: Colors.white,
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.symmetric(horizontal: 2.0, vertical: 1.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+                height: 80.0,
+                width: 330.0,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.amber,
+                      primary: Colors.white,
+                      textStyle: TextStyle(fontSize: 30.0)),
+                  onPressed: () {},
+                  child: Text('Food'),
+                )),
+            SizedBox(
+                height: 80.0,
+                width: 330.0,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.amber,
+                    primary: Colors.white,
+                    textStyle: TextStyle(fontSize: 30.0),
+                  ),
+                  onPressed: () {},
+                  child: Text('Healthcare'),
+                )),
+            SizedBox(
+                height: 80.0,
+                width: 330.0,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.amber,
+                    primary: Colors.white,
+                    textStyle: TextStyle(fontSize: 30.0),
+                  ),
+                  onPressed: () {},
+                  child: Text('Resources'),
+                )),
+            SizedBox(
+                height: 80.0,
+                width: 330.0,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.amber,
+                    primary: Colors.white,
+                    textStyle: TextStyle(fontSize: 30.0),
+                  ),
+                  onPressed: () {},
+                  child: Text('Housing'),
+                )),
+            SizedBox(
+                height: 80.0,
+                width: 330.0,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.amber,
+                    primary: Colors.white,
+                    textStyle: TextStyle(fontSize: 30.0),
+                  ),
+                  onPressed: () {},
+                  child: Text('Education'),
+                )),
+            SizedBox(
+                height: 80.0,
+                width: 330.0,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.amber,
+                    primary: Colors.white,
+                    textStyle: TextStyle(fontSize: 30.0),
+                  ),
+                  onPressed: () {},
+                  child: Text('Others'),
+                )),
+          ],
         ),
-        body: Container(
-          alignment: Alignment.center,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.amber,
-                  primary: Colors.white,
-                ),
-                onPressed: () {},
-                child: Text('Food'),
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.amber,
-                  primary: Colors.white,
-                ),
-                onPressed: () {},
-                child: Text('Healthcare'),
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.amber,
-                  primary: Colors.white,
-                ),
-                onPressed: () {},
-                child: Text('Resources'),
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.amber,
-                  primary: Colors.white,
-                ),
-                onPressed: () {},
-                child: Text('Housing'),
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.amber,
-                  primary: Colors.white,
-                ),
-                onPressed: () {},
-                child: Text('Education'),
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.amber,
-                  primary: Colors.white,
-                ),
-                onPressed: () {},
-                child: Text('Others'),
-              ),
-            ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined, color: Colors.black),
+            backgroundColor: Colors.white,
+            label: '',
           ),
-          //bottomNavigationBar: BottomNavigationBar(
-          //  items: [
-          //    BottomNavigationBarItem(
-          //      icon: Icon(Icons.home),
-          //    )
-          //  ],
-          //  ),
-        ));
+          BottomNavigationBarItem(
+            icon: Icon(Icons.volunteer_activism_outlined, color: Colors.black),
+            backgroundColor: Colors.white,
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle_outline, color: Colors.black),
+            backgroundColor: Colors.white,
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications_outlined, color: Colors.black),
+            backgroundColor: Colors.white,
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_outlined, color: Colors.black),
+            backgroundColor: Colors.white,
+            label: '',
+          )
+        ],
+      ),
+    );
   }
 }
